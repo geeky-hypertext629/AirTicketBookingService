1e4ca46064574c4209966901c697ae1cf1cac98c
geeky-hypertext629 <107639960+geeky-hypertext629@users.noreply.github.com>
2023-08-10 17:32:14 +0530

    SignUp logic and password encryption

--- AuthService/src/index.js
+++ AuthService/src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig.js');
 
+const apiRoutes = require('./routes/index.js');
+
 const prepareAndStartServer = ()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}))
+    app.use('/api', apiRoutes)
+
     app.listen(PORT,()=>{
         console.log(`Server Started om PORT = ${PORT}`)
     })
--- AuthService/src/models/user.js
+++ AuthService/src/models/user.js
@@ -2,6 +2,8 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require('bcrypt')
+const { SALT } = require('../config/serverConfig')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -35,5 +37,12 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'User',
 
   });
+
+  User.beforeCreate((user)=>{
+    // console.log(user);
+    const encryptedPassword = bcrypt.hashSync(user.password,SALT);
+    user.password = encryptedPassword;
+  });
+
   return User;
 };
